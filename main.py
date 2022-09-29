a7e4fc5f5519c59bddb98f7062a76f9fa7a43055
Saveliy <chertkov.03@inbox.ru>
2022-09-29 18:33:40 +0300

    Баг при заполнении таблицы с координатами(((

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPainter, QColor, QPen, QBrush
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 from form import Ui_Form
 
 import sys
@@ -13,8 +13,10 @@ class mywindow(QMainWindow):
         self.ui = Ui_Form()
         self.start_paint = False
         self.paint_pol = True
+        self.dek = True
         self.arrPol_pi = []
-        self.arrPol_dek = []
+        self.arrPol_dek_x = []
+        self.arrPol_dek_y = []
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(lambda: self.buttonClicked())
         self.initUI()
@@ -28,16 +30,19 @@ class mywindow(QMainWindow):
             self.ui.pushButton.setText("Закончить отрисовку")
             self.start_paint = True
             self.paint_pol = False
+            self.dek = False
             self.arrPol_pi.clear()
         else:
             self.ui.pushButton.setText("Начать отрисовку Фигуры")
             self.start_paint = False
             self.paint_pol = True
+            self.dek = True
             self.update()
 
     def mousePressEvent(self, event):
         if self.start_paint:
-            self.arrPol_pi.append(event.pos())
+            pos = event.pos()
+            self.arrPol_pi.append(pos)
 
             self.update()
 
@@ -103,6 +108,18 @@ class mywindow(QMainWindow):
 
         if len(self.arrPol_pi) != 0 and self.paint_pol:
             qp.drawPolygon(self.arrPol_pi)
+
+        if self.dek:
+            for i in range(len(self.arrPol_pi)):
+                self.arrPol_dek_x.append((self.arrPol_pi[i].x() - x0_1) / dx)
+                self.arrPol_dek_y.append((y0 - self.arrPol_pi[i].y()) / dx)
+
+            for i in range(len(self.arrPol_dek_x)):
+                col_count = self.ui.tableWidget.columnCount()
+                col_count += 1
+                self.ui.tableWidget.setColumnCount(col_count)
+                self.ui.tableWidget.setItem(0, i, QTableWidgetItem(str(self.arrPol_dek_x[i])))
+                self.ui.tableWidget.setItem(1, i, QTableWidgetItem(str(self.arrPol_dek_y[i])))
 
         qp.end()
 
